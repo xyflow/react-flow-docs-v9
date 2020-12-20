1ef5a238f71db0cd0364b3207679d9b121ebf08c
moklick <info@moritzklack.com>
2020-12-20 13:59:33 +0100

    feat(examples): layouting

---
+++ src/example-flows/Layouting/index.js
@@ -0,0 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useCallback } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  removeElements,
+  isNode,
+} from 'react-flow-renderer';
+import dagre from 'dagre';
+
+import initialElements from './initial-elements';
+
+import './layouting.css';
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (elements, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+
+  elements.forEach((el) => {
+    if (isNode(el)) {
+      dagreGraph.setNode(el.id, { width: 150, height: 50 });
+    } else {
+      dagreGraph.setEdge(el.source, el.target);
+    }
+  });
+
+  dagre.layout(dagreGraph);
+
+  return elements.map((el) => {
+    if (isNode(el)) {
+      const nodeWithPosition = dagreGraph.node(el.id);
+      el.targetPosition = isHorizontal ? 'left' : 'top';
+      el.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+      // unfortunately we need this little hack to pass a slighltiy different position
+      // in order to notify react flow about the change
+      el.position = {
+        x: nodeWithPosition.x + Math.random() / 1000,
+        y: nodeWithPosition.y,
+      };
+    }
+
+    return el;
+  });
+};
+
+const layoutedElements = getLayoutedElements(initialElements);
+
+const LayoutFlow = () => {
+  const [elements, setElements] = useState(layoutedElements);
+  const onConnect = (params) =>
+    setElements((els) =>
+      addEdge({ ...params, type: 'smoothstep', animated: true }, els)
+    );
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+
+  const onLayout = useCallback(
+    (direction) => {
+      const layoutedElements = getLayoutedElements(elements, direction);
+      setElements(layoutedElements);
+    },
+    [elements]
+  );
+
+  return (
+    <div className="layoutflow">
+      <ReactFlowProvider>
+        <ReactFlow
+          elements={elements}
+          onConnect={onConnect}
+          onElementsRemove={onElementsRemove}
+          connectionLineType="smoothstep"
+        />
+        <div className="controls">
+          <button onClick={() => onLayout('TB')}>vertical layout</button>
+          <button onClick={() => onLayout('LR')}>horizontal layout</button>
+        </div>
+      </ReactFlowProvider>
+    </div>
+  );
+};
+
+export default LayoutFlow;
